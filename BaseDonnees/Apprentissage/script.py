d42367726132e045c95112280be04d04b5d53575
JadeLeaNeav <jade.lea.neav@gmail.com>
2022-05-17 16:33:24 +0200

    rm script

--- BaseDonnees/Apprentissage/script.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def format_str(nb):
-	return "0" * (4 - len(str(nb))) + str(nb)
-
-for i in range(1, 26):
-	name_pre = 't_anima2d_1000 ('+ str(nb) + ').jpg' 
-	name_post = 't_anima2d_'+ format_str(1000 + i) + '.jpg' 
-	os.rename(name_pre, name_post)
