1f479993e05b6b630cf01341f7a09035296c7515
JadeLeaNeav <jade.lea.neav@gmail.com>
2022-05-17 14:56:01 +0200

    suppression d'un script ajouté par erreur

--- BaseDonnees/Apprentissage/script.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def format_str(nb):
-	return "0" * (4 - len(str(nb))) + str(nb)
-
-for i in range(1039, 1047):
-	name_pre = 'stopmot_'+ format_str(i) + '.jpg' 
-	name_post = 'a_stopmot_'+ format_str(i) + '.jpg' 
-	os.rename(name_pre, name_post)
